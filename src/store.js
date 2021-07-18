f78c22e6125193f2af04eaaba185578151774bf7
Nadia Monayong <nadia.monayongmedjo@gmail.com>
2021-07-18 23:19:59 +0200

    remove createSlice (Redux)

--- src/store.js
+++ src/store.js
@@ -1,13 +1,3 @@
-/* import { configureStore } from '@reduxjs/toolkit';
-import songReducer from './songSlice';
-
-export const store = configureStore({
-  reducer: {
-    song: songReducer,
-  },
-});
-export default store;
-*/
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
